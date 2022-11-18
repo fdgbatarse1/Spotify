1e6d1a73d0dfa2108e3c1310f5404fc686660888
Guzmance <00031119@uca.edu.sv>
2022-11-18 00:41:49 -0600

    Update: Some Modal Code Issues Fixed

--- components/Controls/DeletePlaylist/index.tsx
+++ components/Controls/DeletePlaylist/index.tsx
@@ -39,7 +39,7 @@ const DeletePlaylist = ({ id }: { id: string | undefined }) => {
           <p>Do you want to unfollow this playlist?</p>
           <div className="flex justify-around items-center">
             <button
-              className="text-white text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 hover:text-white pointer"
+              className="bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 hover:text-white pointer"
               onClick={() => confirmationHandler(true)}
             >
               Yes
--- components/Controls/RemoveFromPlaylist/index.tsx
+++ components/Controls/RemoveFromPlaylist/index.tsx
@@ -65,7 +65,7 @@ const RemoveFromPlaylist = ({ playlistId, track }: IRemoveFromPlaylist) => {
           <p>Do you want to delete this track from this playlist?</p>
           <div className="flex justify-around items-center">
             <button
-              className="text-white text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 hover:text-white pointer"
+              className="bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 hover:text-white pointer"
               onClick={() => confirmationHandler(true)}
             >
               Yes
