5deee467d222187fae11624cc7fab2fd619fa5c4
Batarse1 <00014419@uca.edu.sv>
2022-11-21 13:25:46 -0600

    :bug: Fix bgColor of sidebar element

--- components/Layout/Sidebar/Button/index.tsx
+++ components/Layout/Sidebar/Button/index.tsx
@@ -1,5 +1,5 @@
-import React, { LegacyRef, MouseEventHandler } from 'react';
-import { useRouter } from 'next/router';
+import React, { LegacyRef, MouseEventHandler } from "react";
+import { useRouter } from "next/router";
 
 interface IButton {
   onClick?: MouseEventHandler<HTMLAnchorElement>;
@@ -8,23 +8,26 @@ interface IButton {
   text: string;
 }
 
-const Button =  React.forwardRef(
-  function Button({ onClick, href, children, text }: IButton, ref: LegacyRef<HTMLAnchorElement>){
-    const router = useRouter();
+const Button = React.forwardRef(function Button(
+  { onClick, href, children, text }: IButton,
+  ref: LegacyRef<HTMLAnchorElement>
+) {
+  const router = useRouter();
 
-    return (
-      <a href={href} onClick={onClick} ref={ref}>
-        <div
-          className={`flex flex-col md:flex-row items-center gap-2 py-2 px-2 font-inter ${
-            router.pathname === href ? 'md:bg-gray-50 dark:bg-gray-300 md:rounded-lg text-green-500' : 'text-gray-500'
-          }`}
-        >
-          {children}
-          <p className={'text-sm md:text-base text-center'}>{text}</p>
-        </div>
-      </a>
-    );
-  },
-);
+  return (
+    <a href={href} onClick={onClick} ref={ref}>
+      <div
+        className={`flex flex-col md:flex-row items-center gap-2 py-2 px-2 font-inter ${
+          router.pathname === href
+            ? "md:bg-gray-50 dark:md:bg-gray-300 md:rounded-lg text-green-500"
+            : "text-gray-500"
+        }`}
+      >
+        {children}
+        <p className={"text-sm md:text-base text-center"}>{text}</p>
+      </div>
+    </a>
+  );
+});
 
 export default Button;
